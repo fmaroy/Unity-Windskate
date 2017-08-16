f999bcc21e12175e7a98a6da102dcc755dd87d6d
fmaroy <fabien_maroy@hotmail.com>
2017-08-16 17:58:19 +0200

    small edits

---
+++ WindSkate/Assets/CinemachineControls.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineControls : MonoBehaviour {
+
+	int numberOfFollowCameras = 2;
+	public bool isIntroScene = false;
+	private RaceManagerScript raceData;
+	// referenced to the player object elements to be tracked;
+	public GameObject playerOrientTarget;
+	public GameObject playerPosTarget;
+	//placeholder objects following the player motions
+	public GameObject playerRefPos;
+	public GameObject playerRefOrient;
+	// following can be gameplay, trackdemo,  or intro
+	//public int cameraStateID = 0;
+	public List<GameObject> GameCameraList = new List<GameObject>();
+	public int gameViewId = 0;
+	public List<GameObject> CrashCameraList = new List<GameObject>();
+	public GameObject OrbitCamera;
+	public CinemachineFreeLook OrbitCameraData;
+	public GameObject camFollowTraceObj;
+	public GameObject cameraContainers;
+	public Animator cameraAnimator;
+	public float introSceneCamTimer = 5; 
+	private float camtimer;
+
+
+	// Use this for initialization
+	void Start () {
+		cameraAnimator = this.GetComponent<Animator> ();
+		raceData = GameObject.Find ("RaceManager").GetComponent<RaceManagerScript>();
+		isIntroScene = raceData.gameObject.GetComponent<UserPreferenceScript> ().IntroScene;
+		camtimer = 0;
+		OrbitCameraData = OrbitCamera.GetComponent<CinemachineFreeLook> ();
+		initCamera ();
+	}
+		
+	public void initCamera ()
+	{
+		CrashCameraHandler (false, 0);
+		GameCameraHandler (0);
+		camFollowTraceObj.SetActive (false);
+	}
+		
+	public void CrashCameraHandler (bool isCrashed, int CrashId)
+	{
+		//Debug.Log ("Camera crash " + isCrashed);
+		if (isCrashed) {
+			// enables the crash camera #i
+			int i = 0;
+			foreach (GameObject camObj in CrashCameraList) {
+				if (CrashId == i) {
+					//Debug.Log ("enables camera : " + camObj.name);
+					camObj.SetActive (true);
+				} else {
+					//Debug.Log ("disables camera : " + camObj.name);
+					camObj.SetActive (false);
+				}
+				i++;
+			}
+		} else {
+			// disables the crash camera
+			//Debug.Log("disables all cameras");
+			foreach (GameObject camObj in CrashCameraList) {
+				//Debug.Log ("disables camera : " + camObj.name);
+				camObj.SetActive (false);
+			}
+		}
+	}
+
+	public void disableOrbitCamera()
+	{
+		OrbitCamera.SetActive(false);
+		OrbitCameraData.m_XAxis.Value = 90f;
+
+	}
+
+	public void orbitCameraHandler(float CamShiftX, float CamShiftY)
+	{
+		// enbales roation camera
+		OrbitCamera.SetActive(true);
+		OrbitCameraData.m_XAxis.Value = CamShiftX;
+		OrbitCameraData.m_YAxis.Value = CamShiftY;
+
+
+
+	}
+
+	public void GameCameraHandler(int id)
+	{
+		Debug.Log ("load game camera : " + id);
+		//CrashCameraHandler (false, 0);
+		int i = 0;
+		foreach (GameObject camObj in GameCameraList) {
+			if (id == i) {
+				Debug.Log ("enables camera : " + camObj.name);
+				camObj.SetActive (true);
+			} else {
+				Debug.Log ("disables camera : " + camObj.name);
+				camObj.SetActive (false);
+			}
+			i++;
+		}
+	}
+
+	public void nextView() {
+		int maximumNumberOfViewpoints = GameCameraList.Count;
+		gameViewId = gameViewId + 1;
+		if (gameViewId >= maximumNumberOfViewpoints) {
+			gameViewId = 0;
+		}
+		GameCameraHandler (gameViewId);
+	}
+
+	// Update is called once per frame
+
+	void FixedUpdate(){
+		playerRefPos.transform.position = playerPosTarget.transform.position;
+
+		playerRefOrient.transform.position = playerOrientTarget.transform.position;
+		playerRefOrient.transform.eulerAngles = new Vector3(0f, playerOrientTarget.transform.eulerAngles.y, 0f);
+	}
+
+	void Update () {
+		
+		if (isIntroScene) 
+		{
+			camtimer = camtimer + Time.deltaTime;
+			if (camtimer > introSceneCamTimer) {
+				camtimer = 0;
+				nextView ();
+			}
+		}
+	}
+}
---
+++ WindSkate/Assets/WindGaugeCircleScript.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindGaugeCircleScript : MonoBehaviour {
+
+	public GameObject windSource;
+	public windEffector windData;
+	public float currentWindAngle;
+	LineRenderer line;
+	Gradient lineGradient;
+	float[] linePos ;
+	public float lineAngularLength = 90f;
+	public float lineAngularResolution = 1f;
+	public GameObject player;
+	public float heightPosition = 0.0f;
+	public float radius = 60f;
+	public int gradientResolution = 4;
+
+	//public Color[] WindAnglesColors = new Color[3];
+	//public List<WindAnglesClass> listOfWindAngles = new List<WindAnglesClass> ();
+
+
+
+	// Use this for initialization
+	void Start () {
+		windData = windSource.GetComponent<windEffector>();
+		line = this.GetComponent<LineRenderer> ();
+		lineGradient = line.colorGradient;
+		currentWindAngle = player.GetComponent<Follow_track>().angleBoardToWind;
+		//linePos = new float[Mathf.RoundToInt(lineAngularLength / lineAngularResolution)];
+	}
+
+
+
+
+	LineRenderer linePosDefinition(LineRenderer l, float centerAngle, float length, int segmentCount, float rad, float height)
+	{
+		float currentMinAngle = centerAngle - length / 2;
+		float currentMaxAngle = centerAngle + length / 2;
+		//Debug.Log ("min : " + currentMinAngle);
+		//Debug.Log ("max : " + currentMaxAngle);
+
+		for (int i = 0; i < segmentCount + 1; i ++)
+		{
+			float lerpValue = (float)i /(float)segmentCount;
+			//Debug.Log ("i : " + i + ", Lerp : " + lerpValue);
+			float currentAngle = Mathf.LerpAngle (currentMinAngle, currentMaxAngle, lerpValue);
+			//Debug.Log (currentAngle);
+			float x = Mathf.Sin (Mathf.Deg2Rad * currentAngle) * rad;
+			float y = Mathf.Cos (Mathf.Deg2Rad * currentAngle) * rad;
+			l.SetPosition (i, new Vector3 (x, y, height));
+		}
+		return l;
+	}
+
+	GradientColorKey[] lineGradientDefinition (float centerAngle, float length, int resolution)
+	{
+		
+		List<GradientColorKey> keyConstruction = new List<GradientColorKey> ();
+		float currentMinAngle = centerAngle - length / 2;
+		float currentMaxAngle = centerAngle + length / 2;
+		int coursetype = transform.parent.GetComponent<CircleIndicators> ().typeOfCourse;
+
+		GradientColorKey currentKey;
+		currentKey.color = transform.parent.GetComponent<CircleIndicators> ().getColorForWindAngle (coursetype, currentMinAngle);
+		currentKey.time = 0;
+		keyConstruction.Add (currentKey);
+
+		currentKey.color = transform.parent.GetComponent<CircleIndicators> ().getColorForWindAngle (coursetype, currentMinAngle);
+		currentKey.time = 1;
+		keyConstruction.Add (currentKey);
+
+		foreach (float angle in transform.parent.GetComponent<CircleIndicators>().listOfWindAngles[coursetype].bestAngles) {
+			if (Mathf.Abs (Mathf.DeltaAngle (centerAngle, angle)) < length / 2) {
+				currentKey.color = transform.parent.GetComponent<CircleIndicators> ().getColorForWindAngle (coursetype, angle);
+				currentKey.time = Mathf.InverseLerp (currentMinAngle, currentMaxAngle, angle);
+				keyConstruction.Add (currentKey);
+			}
+		}
+		foreach (float angle in transform.parent.GetComponent<CircleIndicators>().listOfWindAngles[coursetype].worstAngles) {
+			if (Mathf.Abs (Mathf.DeltaAngle (centerAngle, angle)) < length / 2) 
+			{
+				currentKey.color = transform.parent.GetComponent<CircleIndicators> ().getColorForWindAngle (coursetype, angle);
+				currentKey.time = Mathf.InverseLerp(currentMinAngle, currentMaxAngle, angle);
+				keyConstruction.Add (currentKey);
+			}
+		}
+
+		GradientColorKey[] currentColorGradient = new GradientColorKey [keyConstruction.Count];
+		int i = 0;
+		foreach (GradientColorKey key in keyConstruction) {
+			currentColorGradient [i] = key;
+			i++;
+		}
+		return currentColorGradient;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		currentWindAngle = player.GetComponent<Follow_track>().angleBoardToWind;
+		int segments = Mathf.RoundToInt (lineAngularLength / lineAngularResolution);
+		line.SetVertexCount (segments + 1);
+		// following Makes sure each wraps around 360
+		//currentMinAngle = Mathf.MoveTowardsAngle(currentWindAngle, currentMinAngle, lineAngularLength /2);
+
+
+		//currentMaxAngle = Mathf.MoveTowardsAngle(currentWindAngle, currentMaxAngle, lineAngularLength /2);
+
+		//line.numCapVertices = 5;
+
+		line = linePosDefinition (line, 0, lineAngularLength, segments, radius, heightPosition);
+
+		lineGradient.colorKeys = lineGradientDefinition (currentWindAngle, lineAngularLength, gradientResolution);
+		line.colorGradient = lineGradient;
+
+		transform.eulerAngles = new Vector3 (90.0f,0.0f,-1*windData.localWindDirection - 90);
+	}
+}
