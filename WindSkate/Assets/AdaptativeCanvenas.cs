1ef3a503c1525f5b0dc3e96fc0e3d2c67e62fe54
fmaroy <fabien_maroy@hotmail.com>
2017-12-27 10:18:22 +0100

    new style main menu, adapted for iphone x

---
+++ WindSkate/Assets/AdaptativeCanvenas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdaptativeCanvenas : MonoBehaviour {
+
+	public string deviceType;
+	public RectTransform thisRect;
+
+	// Use this for initialization
+	void Start () {
+		deviceType = SystemInfo.deviceModel;
+		thisRect = this.GetComponent<RectTransform> ();
+		Debug.Log ("AnchorMin: ");
+		Debug.Log (thisRect.anchoredPosition3D);
+		Debug.Log (thisRect.anchorMax);
+		Debug.Log (deviceType);
+		if (deviceType == "iPhone10,6") {
+			thisRect.offsetMin = new Vector2 (50f, 0f);
+			thisRect.offsetMax = new Vector2 (0f, 0f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
